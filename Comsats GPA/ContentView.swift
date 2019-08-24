982b22233faf44c9d658f37f2add60d49f94de32
Mouzzam <moazzamtahir81@gmail.com>
2019-08-25 02:13:07 +0500

    Initial Commit

---
+++ Comsats GPA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Comsats GPA
+//
+//  Created by Moazzam Tahir on 25/08/2019.
+//  Copyright © 2019 Moazzam Tahir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
