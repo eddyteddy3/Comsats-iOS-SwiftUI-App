594b4a7894c246b720e6d8acd9da5ee03489b96b
Mouzzam <moazzamtahir81@gmail.com>
2019-08-25 13:51:18 +0500

    Design Template for first page. with some button and image added

--- Comsats GPA/ContentView.swift
+++ Comsats GPA/ContentView.swift
@@ -10,12 +10,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            ZStack {
+                Circle()
+                    .foregroundColor(.white)
+                    .scaleEffect(0.40)
+                    .background(Color.white)
+                    .shadow(color: Color.black, radius: 10, x: 0, y: 5)
+                Image("comsats")
+                    .scaleEffect(0.35)
+            }.cornerRadius(0).scaleEffect(0.7).frame(height: UIScreen.main.bounds.height/3.5)
+                
+            Spacer()
+            
+            VStack {
+                OperationalButton()
+                OperationalButton()
+                OperationalButton()
+                OperationalButton()
+            }
+            Spacer()
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct OperationalButton: View {
+    var body: some View {
+        Button(action: {
+            print("Button Tapped")
+        }) {
+            Text("Button 1")
+                .foregroundColor(.white)
+                .frame(width: 250, height: 50)
+                .background(Color("color2"))
+                .cornerRadius(40)
+                .shadow(radius: 20)
+                .padding(10)
+        }
     }
 }
