ab35046a36367cf4dafbfd74fb6d4fcaeef8efd0
Mouzzam <moazzamtahir81@gmail.com>
2019-09-26 00:31:25 +0500

    Minor changes

--- Comsats GPA/Grade.swift
+++ Comsats GPA/Grade.swift
@@ -13,19 +13,41 @@ struct Grade: View {
     @State var showSubjectsFields = false
     var body: some View {
         VStack {
-            Text("Enter number of subjects")
-            TextField("Enter number of subjects", text: $numberOfSubjects)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.leading, 80)
-                .padding(.trailing, 80)
-            Button(action: {
+            VStack {
+                Text("Enter number of subjects")
+                TextField("Enter number of subjects", text: $numberOfSubjects)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.leading, 80)
+                    .padding(.trailing, 80)
+                Button(action: {
+                    
+                }) {
+                    Text("Enter")
+                }
+                .foregroundColor(.orange)
+                .frame(width: 100, height: 40)
+                .background(Color(.gray))
                 
-            }) {
-                Text("Enter")
+                
+            }.padding()
+            
+            HStack {
+                Text("Subjects").padding(.trailing, 80)
+                Text("GPA").padding()
+                Text("Credit Hours")
             }
             
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Proceed")
+            }
+            .frame(width: 200, height: 50)
+            .foregroundColor(.orange)
+            .background(Color(.gray))
+            .padding(.bottom)
         }
-        .padding(.top)
+        
     }
 }
 
